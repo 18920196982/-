2138afea5031e52df8de01a52e996b19faa6b778
18920196982 <47854633+18920196982@users.noreply.github.com>
2019-04-02 19:52:42 +0800

    Revert "博彩DAPP"
    This reverts commit dff27a4e0f0c2bc4687afbc87c78727d8ce0ff51.

--- src/lottery.js
+++
@@ -1,4 +0,0 @@
-import web3 from './web3';
-const address = '0xbD1d450D8F62DA1d5E3d8d9bA54aF6d3c8141b2c';
-const  abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"pickwiner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
-export default new web3.eth.Contract(abi,address);
